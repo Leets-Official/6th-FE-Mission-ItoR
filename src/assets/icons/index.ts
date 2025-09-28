a33ec78dec15cf95e6777529d7cf73a9145ba4d3
chaemin <cheamin0038@gachon.ac.kr>
2025-09-28 23:18:04 +0900

    [Refactor] 파일 위치 변경

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,27 @@
+import CreateIcon from "@/assets/icons/create.svg?react";
+import AddPhotoAlternateIcon from "@/assets/icons/add_photo_alternate.svg?react";
+import ChatIcon from "@/assets/icons/chat.svg?react";
+import ClearIcon from "@/assets/icons/clear.svg?react";
+import DeleteForeverIcon from "@/assets/icons/delete_forever.svg?react";
+import DoneIcon from "@/assets/icons/done.svg?react";
+import ErrorOutlineIcon from "@/assets/icons/error_outline.svg?react";
+import FolderOpenIcon from "@/assets/icons/folder_open.svg?react";
+import MoreVertIcon from "@/assets/icons/more_vert.svg?react";
+import NavigateBeforeIcon from "@/assets/icons/navigate_before.svg?react";
+import ReorderIcon from "@/assets/icons/reorder.svg?react";
+import SettingsIcon from "@/assets/icons/settings.svg?react";
+
+export {
+  CreateIcon,
+  AddPhotoAlternateIcon,
+  ChatIcon,
+  ClearIcon,
+  DeleteForeverIcon,
+  DoneIcon,
+  ErrorOutlineIcon,
+  FolderOpenIcon,
+  MoreVertIcon,
+  NavigateBeforeIcon,
+  ReorderIcon,
+  SettingsIcon,
+};
