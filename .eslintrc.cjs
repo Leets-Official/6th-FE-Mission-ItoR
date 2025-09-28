0a9113db9eac31cb2b2c6b94a8c09576c07da6c9
chaemin <cheamin0038@gachon.ac.kr>
2025-09-28 14:30:21 +0900

    [Refactor] prettier 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,18 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "react"],
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
-    ],
-    settings: { react: { version: "detect" } }
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    quotes: ["error", "backtick"],
+    "prefer-template": "off",
+    "prettier/prettier": "error"
+  },
+  settings: {
+    react: { version: "detect" }
+  }
 };
