fa36e6e3eac835c14e73f0ce433ce795d8abf911
chaemin <cheamin0038@gachon.ac.kr>
2025-09-30 00:22:26 +0900

    [Feat] Header 테스트 페이지로 라우팅하는 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,45 +5,45 @@ import TextTest from "./pages/test-page/text-test";
 import ToastTest from "./pages/test-page/toast-test";
 import ModalTest from "./pages/test-page/modal-test";
 import DropdownTest from "./pages/test-page/dropdown-test";
+import HeaderTest from "./pages/test-page/header-test"; // ✅ 추가
 
 function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      {/* ✅ flex-wrap 적용 */}
       <div className="flex max-w-4xl flex-wrap justify-center gap-4">
         <Link to="/avatar-test">
           <button className="w-52 rounded-md bg-indigo-500 px-5 py-3 text-white hover:bg-indigo-600">
             Avatar 테스트 페이지
           </button>
         </Link>
-
         <Link to="/button-test">
           <button className="w-52 rounded-md bg-green-600 px-5 py-3 text-white hover:bg-green-700">
             Button 테스트 페이지
           </button>
         </Link>
-
         <Link to="/text-test">
           <button className="w-52 rounded-md bg-orange-500 px-5 py-3 text-white hover:bg-orange-600">
             TextField 테스트 페이지
           </button>
         </Link>
-
         <Link to="/toast-test">
           <button className="w-52 rounded-md bg-blue-500 px-5 py-3 text-white hover:bg-blue-600">
             Toast 테스트 페이지
           </button>
         </Link>
-
         <Link to="/modal-test">
           <button className="w-52 rounded-md bg-purple-500 px-5 py-3 text-white hover:bg-purple-600">
             Modal 테스트 페이지
           </button>
         </Link>
-
         <Link to="/dropdown-test">
           <button className="w-52 rounded-md bg-pink-500 px-5 py-3 text-white hover:bg-pink-600">
             Dropdown 테스트 페이지
+          </button>
+        </Link>
+        <Link to="/header-test">
+          <button className="w-52 rounded-md bg-yellow-500 px-5 py-3 text-white hover:bg-yellow-600">
+            Header 테스트 페이지
           </button>
         </Link>
       </div>
@@ -61,6 +61,7 @@ function App() {
       <Route path="/toast-test" element={<ToastTest />} />
       <Route path="/modal-test" element={<ModalTest />} />
       <Route path="/dropdown-test" element={<DropdownTest />} />
+      <Route path="/header-test" element={<HeaderTest />} /> {/* ✅ 추가 */}
     </Routes>
   );
 }
