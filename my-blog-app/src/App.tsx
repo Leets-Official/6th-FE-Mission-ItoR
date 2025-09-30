74a5889612f5ddf97ea1ec7bb44c8c6e86c35d5a
Seol <pseol@gachon.ac.kr>
2025-09-30 17:27:31 +0900

    ConfirmModal 공통 컴포넌트 추가 및 Noto Sans KR 폰트 적용

--- my-blog-app/src/App.tsx
+++ my-blog-app/src/App.tsx
@@ -1,22 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ButtonTestPage from "./pages/ButtonTestPage";
-import ToastTestPage from "./pages/ToastTestPage";
-import MenuTestPage from "./pages/MenuTestPage";
-import TextCardTestPage from "./pages/TextCardTestPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ButtonTestPage from './pages/ButtonTestPage'
+import ToastTestPage from './pages/ToastTestPage'
+import MenuTestPage from './pages/MenuTestPage'
+import TextCardTestPage from './pages/TextCardTestPage'
+import ConfirmModalTestPage from './pages/ConfirmModalTestPage'
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>홈 화면</div>} />
-        <Route path="/test/button" element={<ButtonTestPage />} />
-        <Route path="/test/toast" element={<ToastTestPage />} />
-        <Route path="/test/menu" element={<MenuTestPage />} />
-        <Route path="/test/textcard" element={<TextCardTestPage />} />
+        <Route path='/' element={<div>홈 화면</div>} />
+        <Route path='/test/button' element={<ButtonTestPage />} />
+        <Route path='/test/toast' element={<ToastTestPage />} />
+        <Route path='/test/menu' element={<MenuTestPage />} />
+        <Route path='/test/textcard' element={<TextCardTestPage />} />
+        <Route path='/test/confirmmodal' element={<ConfirmModalTestPage />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
 export default App
--- my-blog-app/src/pages/TextCardTestPage.tsx
+++ my-blog-app/src/pages/TextCardTestPage.tsx
@@ -1,26 +1,22 @@
-import TextCard from "../components/common/TextCard";
+import TextCard from '../components/common/TextCard'
 
 export default function TextCardTestPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-900 p-8">
+    <div className='min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-900 p-8'>
       {/* 1번째 스타일 */}
-      <TextCard
-        variant="primary"
-        title="32 Title one line"
-        subtitle="subtitle one line"
-      />
+      <TextCard variant='primary' title='32 Title one line' subtitle='subtitle one line' />
 
       {/* 2번째 스타일 */}
       <TextCard
-        variant="secondary"
-        title="16 Title one line"
-        subtitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        variant='secondary'
+        title='16 Title one line'
+        subtitle='Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
       />
 
       {/* 3번째 스타일 */}
-      <TextCard variant="body">
+      <TextCard variant='body'>
         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       </TextCard>
     </div>
-  );
+  )
 }
