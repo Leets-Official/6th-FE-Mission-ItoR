28c95f5c280bc51da5737b474c2b7991083da323
chaemin <cheamin0038@gachon.ac.kr>
2025-09-30 17:46:22 +0900

    [Feat] 라우터 구성 및 MainPage 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,13 @@
-import { Routes, Route, Link } from "react-router-dom";
-import AvatarTest from "./pages/test-page/avatar-test";
-import ButtonTest from "./pages/test-page/button-test";
-import TextTest from "./pages/test-page/text-test";
-import ToastTest from "./pages/test-page/toast-test";
-import ModalTest from "./pages/test-page/modal-test";
-import DropdownTest from "./pages/test-page/dropdown-test";
-import HeaderTest from "./pages/test-page/header-test";
-import SidebarTest from "./pages/test-page/sidebar-test";
-import PaginationTest from "./pages/test-page/pagination-test";
-
-function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="flex max-w-4xl flex-wrap justify-center gap-4">
-        <Link to="/avatar-test">
-          <button className="w-52 rounded-md bg-indigo-500 px-5 py-3 text-white hover:bg-indigo-600">
-            Avatar 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/button-test">
-          <button className="w-52 rounded-md bg-green-600 px-5 py-3 text-white hover:bg-green-700">
-            Button 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/text-test">
-          <button className="w-52 rounded-md bg-orange-500 px-5 py-3 text-white hover:bg-orange-600">
-            TextField 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/toast-test">
-          <button className="w-52 rounded-md bg-blue-500 px-5 py-3 text-white hover:bg-blue-600">
-            Toast 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/modal-test">
-          <button className="w-52 rounded-md bg-purple-500 px-5 py-3 text-white hover:bg-purple-600">
-            Modal 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/dropdown-test">
-          <button className="w-52 rounded-md bg-pink-500 px-5 py-3 text-white hover:bg-pink-600">
-            Dropdown 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/header-test">
-          <button className="w-52 rounded-md bg-yellow-500 px-5 py-3 text-white hover:bg-yellow-600">
-            Header 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/sidebar-test">
-          <button className="w-52 rounded-md bg-teal-500 px-5 py-3 text-white hover:bg-teal-600">
-            Sidebar 테스트 페이지
-          </button>
-        </Link>
-        <Link to="/pagination-test">
-          <button className="w-52 rounded-md bg-red-500 px-5 py-3 text-white hover:bg-red-600">
-            Pagination 테스트 페이지
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-}
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import Playground from "./pages/test-page/Playground";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/avatar-test" element={<AvatarTest />} />
-      <Route path="/button-test" element={<ButtonTest />} />
-      <Route path="/text-test" element={<TextTest />} />
-      <Route path="/toast-test" element={<ToastTest />} />
-      <Route path="/modal-test" element={<ModalTest />} />
-      <Route path="/dropdown-test" element={<DropdownTest />} />
-      <Route path="/header-test" element={<HeaderTest />} />
-      <Route path="/sidebar-test" element={<SidebarTest />} />
-      <Route path="/pagination-test" element={<PaginationTest />} />
+      <Route path="/" element={<MainPage />} />
+
+      <Route path="/playground/*" element={<Playground />} />
     </Routes>
   );
 }
