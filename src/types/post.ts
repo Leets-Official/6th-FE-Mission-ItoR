72cfbbb6cc985d1405a475f28b388246dc469337
chaemin <cheamin0038@gachon.ac.kr>
2025-09-30 17:46:11 +0900

    [Type] 게시글 타입 정의 추가

---
+++ src/types/post.ts
@@ -0,0 +1,25 @@
+export type ContentType = "TEXT" | "IMAGE";
+
+export interface PostContent {
+  contentOrder: number;
+  content: string;
+  contentType: ContentType;
+}
+
+export interface Comment {
+  commentId: number;
+  content: string;
+  nickName: string;
+  isOwner: boolean;
+}
+
+export interface Post {
+  postId: string;
+  title: string;
+  contents: PostContent[];
+  isOwner: boolean;
+  comments: Comment[];
+  nickName: string;
+  profileUrl: string;
+  createdAt: string;
+}
