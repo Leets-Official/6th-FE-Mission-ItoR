e38d2d431b506288c2cee016d77c8c44321870e4
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-10-06 17:07:11 +0900

    rename: constants 파일명 대소문자 변경

--- src/constants/sidebar.Constants.ts
+++ src/constants/sidebar.Constants.ts
@@ -1,14 +1,15 @@
 export const SIDEBAR_TEXTS = {
-  LOGGED_IN: {
-    MY_GITLOG: '나의 깃로그',
-    WRITE_GITLOG: '깃로그 쓰기',
-    SETTINGS: '설정',
-    LOGOUT: '로그아웃',
-  },
-
-  // 비로그인 상태 텍스트
-  NOT_LOGGED_IN: {
-    QUOTE: 'You can make anything by writing',
-    START_GITLOG: '깃로그 시작하기',
-  },
-} as const;
+    LOGGED_IN: {
+      MY_GITLOG: '나의 깃로그',
+      WRITE_GITLOG: '깃로그 쓰기',
+      SETTINGS: '설정',
+      LOGOUT: '로그아웃',
+    },
+  
+    // 비로그인 상태 텍스트
+    NOT_LOGGED_IN: {
+      QUOTE: 'You can make anything by writing',
+      START_GITLOG: '깃로그 시작하기',
+    },
+  } as const;
+  
