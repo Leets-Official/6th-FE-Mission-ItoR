265fbf8b45abeac6c5711dde72927ca0f8f9f66f
chaemin <cheamin0038@gachon.ac.kr>
2025-10-07 14:11:03 +0900

    [Feat] 요청 인터셉터 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,27 @@
 import axios from "axios";
 
+export interface ApiResponse<T> {
+  code: number;
+  message: string;
+  data: T;
+}
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
