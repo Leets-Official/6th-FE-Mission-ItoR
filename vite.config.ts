0d137e1822a764042f0735b33a01d946415d1730
HandoA01 <chikomhi4@gachon.ac.kr>
2025-10-09 01:56:24 +0900

    icons.ts vite4버전 문법 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,22 @@
 //   plugins: [react()],
 // })
 
+
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
+    svgr(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), 
+    },
+  },
 })
