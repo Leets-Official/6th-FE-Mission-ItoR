001336875670f30c8d9c26cd6b784a1696bad295
Seol <pseol@gachon.ac.kr>
2025-10-11 00:35:56 +0900

    [Refactor] Dropdown: 컴포넌트 통합

--- my-blog-app/src/pages/DropdownTestPage.tsx
+++ my-blog-app/src/pages/DropdownTestPage.tsx
@@ -1,9 +1,7 @@
-import { ArrowDropdownMenu } from '../components/Dropdown/ArrowDropdownMenu'
-import { MaterialDropdownMenu } from '../components/Dropdown/MaterialDropdownMenu'
+import DropdownMenu from '../components/Dropdown/DropdownMenu'
 
 export default function DropdownTestPage() {
   const oneItem = [{ label: 'menu 1', onClick: () => alert('menu 1 클릭') }]
-
   const twoItems = [
     { label: 'menu 1', onClick: () => alert('menu 1 클릭') },
     { label: 'menu 2', onClick: () => alert('menu 2 클릭') },
@@ -13,12 +11,9 @@ export default function DropdownTestPage() {
     <div className='min-h-screen flex flex-col items-center justify-center gap-6 bg-gray-900'>
       <h1 className='text-xl font-bold mb-4 text-white'>Dropdown Menu Test</h1>
 
-      {/* Material Dropdown */}
-      <MaterialDropdownMenu items={twoItems} />
-
-      {/* Arrow Dropdown (1개, 2개) */}
-      <ArrowDropdownMenu items={oneItem} />
-      <ArrowDropdownMenu items={twoItems} />
+      <DropdownMenu items={twoItems} variant='material' />
+      <DropdownMenu items={oneItem} variant='arrow' />
+      <DropdownMenu items={twoItems} variant='arrow' />
     </div>
   )
 }
