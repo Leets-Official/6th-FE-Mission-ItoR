40d71b45c5233e53a05e2c7e5a45987cfd4caba6
Seol <pseol@gachon.ac.kr>
2025-10-11 13:47:54 +0900

    [Refactor] Menu 컴포넌트 리팩토링 - 인라인 SVG를 TriangleIcon 컴포넌트로 분리하여 재사용성 및 가독성 개선 - MenuProps 타입 추가로 타입 안정성 확보

--- my-blog-app/src/components/common/Menu.tsx
+++ my-blog-app/src/components/common/Menu.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react'
+import { TriangleIcon } from '@/assets/icons/TriangleIcon'
 
 type MenuProps = {
   children?: ReactNode
@@ -7,21 +8,11 @@ type MenuProps = {
 export default function Menu({ children }: MenuProps) {
   return (
     <div className='relative inline-flex flex-col items-center'>
-      {/* 사각형 영역 */}
       <div className='flex items-center gap-[10px] px-4 py-1 rounded bg-white shadow-md'>
         {children}
       </div>
 
-      {/* 삼각형 (SVG 그대로 사용 가능) */}
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='16'
-        height='8'
-        viewBox='0 0 16 8'
-        className='absolute -bottom-2 fill-white'
-      >
-        <path d='M8 8L0 0L16 0L8 8Z' />
-      </svg>
+      <TriangleIcon className='absolute -bottom-2 fill-white' />
     </div>
   )
 }
