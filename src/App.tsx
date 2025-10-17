c1412a41603ab7eed89e94975b75e1ca4ce30381
HandoA01 <chikomhi4@gachon.ac.kr>
2025-10-17 20:50:52 +0900

    feat : TextFieldSet 컴포넌트 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import TestTextFieldSet from "./components/testusecomponents/useTextFieldSet";
 
 function App() {
   return (
     <>
-    
+      <TestTextFieldSet/>
     </>
   )
 }
---
+++ src/components/TextFieldSet.tsx
@@ -0,0 +1,40 @@
+// src/components/TextFieldSet.tsx
+import React from "react";
+import TextField from "./TextField";
+
+interface TextFieldSetProps {
+  label: string; // 제목
+  value?: string;
+  onChange?: (value: string) => void;
+  variant?: "gray" | "blue" | "black" | "filled" | "outlineblack";
+  size?: "sm" | "lg";
+  placeholder?: string;
+}
+
+const TextFieldSet: React.FC<TextFieldSetProps> = ({
+  label,
+  value,
+  onChange,
+  variant = "gray",
+  size = "sm",
+  placeholder,
+}) => {
+  return (
+    <div className="w-[688px] h-[104px] p-[12px_16px] flex flex-col gap-[12px]">
+      {/* 제목 */}
+      <span className="font-[Noto Sans KR] font-light text-[14px] leading-[160%] tracking-[-0.5%] text-gray-800">
+        {label}
+      </span>
+      {/* 기존 TextField */}
+      <TextField
+        value={value}
+        onChange={(e) => onChange?.(e.target.value)}
+        variant={variant}
+        size={size}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default TextFieldSet;
