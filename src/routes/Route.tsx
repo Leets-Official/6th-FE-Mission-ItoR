dbd3c95d419e689cd70b4096a4a732c688b071bf
Saelyeam <eileen4505@gachon.ac.kr>
2025-10-19 22:36:59 +0900

    feat:[3주차] WritePage 구현

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -4,12 +4,14 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
 import PostDetailPage from '../pages/PostDetailPage';
+import WritePage from '../pages/WritePage';
 
 
 // 라우터 "객체" 생성
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/post/:id', element: <PostDetailPage /> },
+  { path: '/write', element: <WritePage /> },
   { path: '*', element: <Navigate to="/" replace /> },
 ]);
 
