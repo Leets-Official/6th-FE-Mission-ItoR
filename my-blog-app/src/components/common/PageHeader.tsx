5f1baaedbc867fa6ee65e90733d5c5e8bd14c559
Seol <pseol@gachon.ac.kr>
2025-10-21 21:00:32 +0900

    [Feat] 블로그 작성페이지 구현 (4주차)

--- my-blog-app/src/components/common/PageHeader.tsx
+++ my-blog-app/src/components/common/PageHeader.tsx
@@ -1,25 +1,41 @@
 import type { ReactNode } from 'react'
 import { MenuIcon } from '../../assets/icons/MenuIcon'
+
 type PageHeaderProps = {
   title: string
-  rightContent?: React.ReactNode
+  rightContent?: ReactNode
+  subContent?: ReactNode
+  hideMenu?: boolean
 }
 
-export default function PageHeader({ title, rightContent }: PageHeaderProps) {
+export default function PageHeader({
+  title,
+  rightContent,
+  subContent,
+  hideMenu = false,
+}: PageHeaderProps) {
   return (
-    <header
-      className='
-        flex items-center justify-between
-        w-full px-3 py-4
-        bg-white/90 backdrop-blur-sm
-      '
-    >
-      <div className='flex items-center justify-center gap-2'>
-        <MenuIcon />
-        <h1 className='text-[20px] leading-[28px] font-normal font-smooch text-black'>{title}</h1>
+    <header className='flex flex-col w-full bg-white/90 shadow-[0_4px_4px_rgba(0,0,0,0.01)] backdrop-blur-[2px]'>
+      {/* 상단 헤더 */}
+      <div className='flex justify-between items-center w-full px-3 py-4'>
+        <div className='flex items-center gap-2'>
+          {!hideMenu && <MenuIcon />}
+          <h1 className='text-[20px] leading-[28px] font-normal font-smooch text-black'>{title}</h1>
+        </div>
+        {rightContent && <div className='flex items-center gap-2'>{rightContent}</div>}
       </div>
 
-      <div className='flex items-center gap-2'>{rightContent}</div>
+      {/* 하단 보조 subContent */}
+      {subContent && (
+        <div
+          className='
+            flex justify-center items-center
+            gap-1 px-2 pt-[2px] pb-1 rounded-[2px]
+          '
+        >
+          {subContent}
+        </div>
+      )}
     </header>
   )
 }
