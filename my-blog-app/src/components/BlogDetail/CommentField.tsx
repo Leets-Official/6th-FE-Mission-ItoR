692e080c35926b5019ff1cbd8482dc42b9f1c023
Seol <pseol@gachon.ac.kr>
2025-10-28 15:13:17 +0900

    [Refactor] tailwindcss 클래스 적용

--- my-blog-app/src/components/BlogDetail/CommentField.tsx
+++ my-blog-app/src/components/BlogDetail/CommentField.tsx
@@ -17,10 +17,10 @@ export default function CommentField({
 }: CommentFieldProps) {
   if (state === 'beforeLogin') {
     return (
-      <div className='flex flex-col justify-center items-center self-stretch max-w-[688px] py-3 px-4 gap-[10px] rounded-[4px] border border-[#E6E6E6] h-[66px]'>
+      <div className='flex flex-col justify-center items-center self-stretch max-w-[688px] py-3 px-4 gap-2.5 rounded-[4px] border border-[#E6E6E6] h-[66px]'>
         <TextCard
           variant='body'
-          className='flex justify-center items-center max-w-[688px] py-3 px-4 gap-[10px] self-stretch text-[#333] text-[14px] font-light leading-[160%] tracking-[-0.07px]'
+          className='flex justify-center items-center max-w-[688px] py-3 px-4 gap-2.5 self-stretch text-[#333] text-[14px] font-light leading-[160%] tracking-[-0.07px]'
         >
           로그인을 하고 댓글을 달아보세요!
         </TextCard>
@@ -33,7 +33,7 @@ export default function CommentField({
       <CommentMetaInfo author='닉네임' date='' />
       <TextCard
         variant='body'
-        className='flex flex-col items-start gap-[10px] self-stretch py-2 rounded-[4px] border border-[#E6E6E6] text-[#909090] text-[14px] font-light px-4 h-[112px]'
+        className='flex flex-col items-start gap-2.5 self-stretch py-2 rounded-[4px] border border-[#E6E6E6] text-[#909090] text-[14px] font-light px-4 h-[112px]'
       >
         <textarea
           placeholder='댓글을 입력하세요.'
@@ -43,7 +43,7 @@ export default function CommentField({
         />
       </TextCard>
 
-      <div className='flex justify-end gap-[10px] px-4 py-2'>
+      <div className='flex justify-end gap-2.5 px-4 py-2'>
         <Button intent={state === 'writing' ? 'black' : 'secondary'} size='md' onClick={onSubmit}>
           등록
         </Button>
