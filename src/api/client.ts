f29d8db76fb66685e9dd267244a876b7cafd585b
Saelyeam <eileen4505@gachon.ac.kr>
2025-11-01 22:51:21 +0900

    feat(api): env 기반 baseURL 적용 및 응답 에러 메시지 인터셉터 추가

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
+import { API_BASE_URL } from "@src/utils/env";
 
 const api = axios.create({
-  baseURL: "https://blog.leets.land", 
-  headers: {
-    "Content-Type": "application/json",
-  },
-  withCredentials: true,
+  baseURL: API_BASE_URL,
+  headers: { "Content-Type": "application/json" },
 });
+
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    const msg =
+      error?.response?.data?.message ||
+      error?.response?.data?.responseMessage ||
+      error?.message ||
+      "요청 처리 중 오류가 발생했습니다.";
+    return Promise.reject(new Error(msg));
+  }
+);
 
 export default api;
