5a74be0ed8262f83ac310ddad33b26666ae84cb8
HandoA01 <chikomhi4@gachon.ac.kr>
2025-11-06 06:09:10 +0900

    feat : usePosts 훅

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,44 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  createPost,
+  updatePost,
+  deletePost,
+  getAllPosts,
+  getPostById,
+  type PostBody,
+  type PostResponse,   
+} from "@src/api/posts";
+
+/** 게시글 생성 */
+export const useCreatePost = () =>
+  useMutation({
+    mutationFn: (body: PostBody) => createPost(body),
+  });
+
+/** 게시글 수정 */
+export const useUpdatePost = () =>
+  useMutation({
+    mutationFn: ({ id, body }: { id: number; body: PostBody }) =>
+      updatePost(id, body),
+  });
+
+/** 게시글 삭제 */
+export const useDeletePost = () =>
+  useMutation({
+    mutationFn: (id: number) => deletePost(id),
+  });
+
+/** 게시글 전체 조회 */
+export const useAllPosts = () =>
+  useQuery<PostResponse[]>({
+    queryKey: ["posts"],
+    queryFn: getAllPosts,
+  });
+
+/** 게시글 단일 조회 */
+export const usePostDetail = (id: number) =>
+  useQuery<PostResponse>({
+    queryKey: ["post", id],
+    queryFn: () => getPostById(id),
+    enabled: !!id,
+  });
