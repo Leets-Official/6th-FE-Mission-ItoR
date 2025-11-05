5ffc92bd3423d9a580ff2c314a31e335bd1d34f9
HandoA01 <chikomhi4@gachon.ac.kr>
2025-11-06 06:41:12 +0900

    feat : write API 연동 및 네트워크 응답 확인

--- vite.config.ts
+++ vite.config.ts
@@ -6,23 +6,23 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+
   resolve: {
     alias: {
-    "@": path.resolve(__dirname, "./src"),
-    "@src": path.resolve(__dirname, "./src"),
-    "@api": path.resolve(__dirname, "./src/api"),
-    "@hooks": path.resolve(__dirname, "./src/hooks"),
-    "@components": path.resolve(__dirname, "./src/components"),
-    "@assets": path.resolve(__dirname, "./src/assets"),
+      "@": path.resolve(__dirname, "./src"),
+      "@src": path.resolve(__dirname, "./src"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
-});
 
   server: {
     port: 3000,
     proxy: {
       "/auth": {
-        target: "https://blog.leets.land", // 백엔드 주소
+        target: "https://blog.leets.land",
         changeOrigin: true,
         secure: true,
       },
