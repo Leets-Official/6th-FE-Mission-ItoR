087b488287458bb8a15fb674e8a81c8c91aca882
chaemin <cheamin0038@gachon.ac.kr>
2025-11-11 18:29:07 +0900

    [Feat] HeaderLegacy 컴포넌트에 클릭 이벤트 핸들러 추가

--- src/components/Header/HeaderLegacy.tsx
+++ src/components/Header/HeaderLegacy.tsx
@@ -2,25 +2,30 @@ import { AddPhotoAlternateIcon, FolderOpenIcon } from "@/assets/icons/index";
 import { legacyHeader, legacyButtonGroup, legacyButton } from "./Header.styled";
 
 interface HeaderLegacyProps {
-  showPhotoButton?: boolean; // 사진 추가 버튼 표시 여부
-  showFileButton?: boolean; // 파일 추가 버튼 표시 여부
+  showPhotoButton?: boolean;
+  showFileButton?: boolean;
+  onPhotoClick?: () => void;
+  onFileClick?: () => void;
 }
 
 const HeaderLegacy: React.FC<HeaderLegacyProps> = ({
   showPhotoButton = true,
   showFileButton = true,
+  onPhotoClick,
+  onFileClick,
 }) => {
   return (
     <div className={legacyHeader}>
       <div className={legacyButtonGroup}>
         {showPhotoButton && (
-          <button className={legacyButton}>
+          <button className={legacyButton} type="button" onClick={onPhotoClick}>
             <AddPhotoAlternateIcon className="text-brand-gray h-3 w-3" />
             사진 추가하기
           </button>
         )}
+
         {showFileButton && (
-          <button className={legacyButton}>
+          <button className={legacyButton} type="button" onClick={onFileClick}>
             <FolderOpenIcon className="text-brand-gray h-3 w-3" />
             파일 추가하기
           </button>
