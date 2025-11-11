e926c0ef2963c2d174bce740b3cd0cad860f297e
chaemin <cheamin0038@gachon.ac.kr>
2025-11-11 18:28:45 +0900

    [Refactor] 댓글 스타일 구조 간소화 및 불필요 클래스 제거

--- src/components/Blog/CommentSection/CommentSection.styled.ts
+++ src/components/Blog/CommentSection/CommentSection.styled.ts
@@ -4,7 +4,6 @@ export const title = "text-lg font-medium mb-4 flex items-center gap-1 w-full ma
 export const count = "text-brand-blue text-sm font-normal";
 
 // 댓글 리스트
-export const commentList = "mt-6 flex flex-col gap-16 w-full max-w-[656px]";
 export const commentItemWrapper = "flex w-full max-w-[656px] gap-2";
 export const commentItemContent = "flex flex-1 flex-col";
 export const commentHeader = "flex justify-between items-start";
@@ -17,25 +16,10 @@ export const menuWrapper = "relative";
 export const dropdownPosition = "absolute right-0 top-5 z-50";
 
 // 댓글 입력
-export const commentInputSection = "mt-20";
 export const commentWrapper =
   "w-full max-w-[656px] flex flex-col bg-brand-white border border-brand-lightGray rounded-[4px] p-2 gap-2";
 export const commentProfile = "flex items-center gap-2 px-2 py-2";
 export const commentBox = "flex flex-1 flex-col gap-3 px-2 pb-3";
-export const commentInput =
-  "w-full min-h-[106px] resize-none rounded-md p-2 text-sm text-brand-darkGray placeholder:text-brand-gray";
-export const buttonWrapper = "mt-2 flex justify-end";
 
 // 비로그인 상태
-export const emptyBox =
-  "w-full max-w-[656px] flex flex-col items-center justify-center text-center";
 export const subText = "text-brand-textGray text-sm mb-6";
-export const emptyTextarea =
-  "w-full h-[106px] rounded-[4px] border border-brand-lightGray bg-brand-white flex items-center px-6 text-[15px] text-left text-brand-gray";
-
-// 하단 작성자 정보
-export const footerWrapper =
-  "relative left-1/2 right-1/2 -mx-[50vw] w-screen bg-brand-borderGray mt-10";
-export const footerProfile = "max-w-[720px] mx-auto px-10 py-10 flex flex-col items-start gap-5";
-export const footerNick = "text-xl font-medium";
-export const footerIntro = "text-sm text-brand-gray";
