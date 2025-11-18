f43d7c6d05e7d7386c9700da308b9d4bfeafc3ed
chaemin <cheamin0038@gachon.ac.kr>
2025-11-18 16:04:49 +0900

    [Refactor] 미사용 ApiResponse 인터페이스 삭제

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-export interface ApiResponse<T> {
-  code: number;
-  message: string;
-  data: T;
-}
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
