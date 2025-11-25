f3f93e0f8d0515d3226bc81bd2c9ae4ee45d3bd7
Seol <pseol@gachon.ac.kr>
2025-11-25 14:37:19 +0900

    [Fix] App.tsx 수정(렌더링 오류 해결)

--- my-blog-app/src/App.tsx
+++ my-blog-app/src/App.tsx
@@ -1,12 +1,10 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './routes'
-import { ToastProvider } from '@/context/ToastContext'
-import { ModalProvider } from '@/context/ModalContext'
-import { AuthProvider } from '@/context/AuthContext'
-import { SidebarProvider } from '@/context/SidebarContext'
-import { LoginModalProvider } from '@/context/LoginModalContext'
-import SidebarOverlay from '@/components/Sidebar/SidebarOverlay'
 import { Outlet } from 'react-router-dom'
+import { ToastProvider } from './context/ToastContext'
+import { ModalProvider } from './context/ModalContext'
+import { AuthProvider } from './context/AuthContext'
+import { SidebarProvider } from './context/SidebarContext'
+import { LoginModalProvider } from './context/LoginModalContext'
+import SidebarOverlay from './components/Sidebar/SidebarOverlay'
 
 function App() {
   return (
@@ -15,7 +13,9 @@ function App() {
         <ModalProvider>
           <SidebarProvider>
             <LoginModalProvider>
+              {/* SidebarOverlay는 전역으로 렌더링되며 */}
               <SidebarOverlay />
+              {/* Outlet은 라우터에서 정의된 하위 컴포넌트를 렌더링합니다. */}
               <Outlet />
             </LoginModalProvider>
           </SidebarProvider>
--- my-blog-app/src/hooks/useBlogWrite.ts
+++ my-blog-app/src/hooks/useBlogWrite.ts
@@ -64,7 +64,6 @@ export function useBlogWrite() {
     }
   }
 
-
   return {
     title,
     setTitle,
