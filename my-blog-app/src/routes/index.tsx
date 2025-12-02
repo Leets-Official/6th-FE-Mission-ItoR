714fbbd8611bddd12f18d16fd12a0c3e7e0714ad
Seol <pseol@gachon.ac.kr>
2025-12-02 13:36:32 +0900

    [Refactor] 테스트 페이지 삭제

--- my-blog-app/src/routes/index.tsx
+++ my-blog-app/src/routes/index.tsx
@@ -9,20 +9,6 @@ import BlogWritePage from '@/pages/BlogWritePage'
 import SettingsPage from '@/pages/SettingsPage'
 import AuthKakaoCallback from '@/pages/AuthKakaoCallback'
 import RootLayout from '@/layouts/RootLayout'
-
-// test pages
-import ButtonTestPage from '@/pages/test/ButtonTestPage'
-import ToastTestPage from '@/pages/test/ToastTestPage'
-import MenuTestPage from '@/pages/test/MenuTestPage'
-import TextCardTestPage from '@/pages/test/TextCardTestPage'
-import ConfirmModalTestPage from '@/pages/test/ConfirmModalTestPage'
-import HeaderTestPage from '@/pages/test/HeaderTestPage'
-import DropdownTestPage from '@/pages/test/DropdownTestPage'
-import TextFiledTestPage from '@/pages/test/TextFiledTestPage'
-import TextFiledSetTestPage from '@/pages/test/TextFiledSetTestPage'
-import ProfileImageTestPage from '@/pages/test/ProfileImageTestPage'
-import SidebarTestPage from '@/pages/test/SidebarTestPage'
-import PaginationTestPage from '@/pages/test/PaginationTestPage'
 import ProfilePage from '@/pages/ProfilePage'
 
 const router = createBrowserRouter([
@@ -45,20 +31,6 @@ const router = createBrowserRouter([
           { path: 'blogwrite/:postId', element: <BlogWritePage /> },
           { path: 'profile', element: <ProfilePage /> },
           { path: 'settings', element: <SettingsPage /> },
-
-          // 테스트 페이지들
-          { path: 'test/button', element: <ButtonTestPage /> },
-          { path: 'test/toast', element: <ToastTestPage /> },
-          { path: 'test/menu', element: <MenuTestPage /> },
-          { path: 'test/textcard', element: <TextCardTestPage /> },
-          { path: 'test/confirmmodal', element: <ConfirmModalTestPage /> },
-          { path: 'test/header', element: <HeaderTestPage /> },
-          { path: 'test/dropdown', element: <DropdownTestPage /> },
-          { path: 'test/textfiled', element: <TextFiledTestPage /> },
-          { path: 'test/textfiledset', element: <TextFiledSetTestPage /> },
-          { path: 'test/profileimage', element: <ProfileImageTestPage /> },
-          { path: 'test/sidebar', element: <SidebarTestPage /> },
-          { path: 'test/pagination', element: <PaginationTestPage /> },
         ],
       },
     ],
